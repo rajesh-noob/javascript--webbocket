8f37fe79e579e23c1ebba7de4cca4ff4a3d15ed8
rajesh-noob <152269644+rajesh-noob@users.noreply.github.com>
2023-11-28 10:18:34 +0000

    introducation file saved

---
+++ 01-basics/test.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+/*
+1-javascript is a high scripting long uage to use on developing web and mobile application efficienting.
+2-we used javascript on client side as well as server side.
+3-in javascript we do logical implementation in web pages.
+4-in today rea we use Esb version of javascript,as it is latest one.
+*ES6-Echma script 6 (vanilla javascript)
+
+
+
+
+*/
